e0dd20546c009c5b0f8c71ad29200b06b47fb718
Scott Lahteine <github@thinkyhead.com>
2018-02-01 03:19:30 -0600

    Fix broken G29 W I J

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -4495,7 +4495,8 @@ void home_all_axes() { gcode_G28(true); }
 
       #if ENABLED(AUTO_BED_LEVELING_BILINEAR)
 
-        if (parser.seen('W')) {
+        const bool seen_w = parser.seen('W');
+        if (seen_w) {
           if (!leveling_is_valid()) {
             SERIAL_ERROR_START();
             SERIAL_ERRORLNPGM("No bilinear grid");
@@ -4533,10 +4534,14 @@ void home_all_axes() { gcode_G28(true); }
           return;
         } // parser.seen('W')
 
+      #else
+
+        constexpr bool seen_w = false;
+
       #endif
 
       // Jettison bed leveling data
-      if (parser.seen('J')) {
+      if (!seen_w && parser.seen('J')) {
         reset_bed_level();
         return;
       }
